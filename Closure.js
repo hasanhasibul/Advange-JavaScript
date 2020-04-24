0170179281b597ef98d0d07ed167000c123654d8
hasanhasibul <hasanhasibul395@gmail.com>
2020-04-25 05:40:22 +0600

    some changes

--- Closure.js
+++ Closure.js
@@ -23,4 +23,6 @@ console.log(clock2());
 console.log(clock1());
 console.log(clock2());
 
-// when function return another function ,then function create a environment that's called closure
+// when function return another function ,then function create a environment that's called closure.
+// When you create an internal scope but can’t access from outside
+// Call a function inside a function or return a function from another
