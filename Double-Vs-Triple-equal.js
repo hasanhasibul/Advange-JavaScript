cb6fbe9103f70222dccabde292f34a6377e45f42
hasanhasibul <hasanhasibul395@gmail.com>
2020-04-25 00:49:49 +0600

    Double Vs triple equal

---
+++ Double-Vs-Triple-equal.js
@@ -0,0 +1,21 @@
+// const a =2;
+// const b ="2";
+// if (a==b) // == check only value.Or try to make same type and check value
+// {
+//     console.log("this is true");
+    
+// } else {
+//     console.log("this is false");
+    
+// }
+
+const a =2;
+const b ="2";
+if (a===b) // === stricklly check only value and type 
+{
+    console.log("this is true");
+    
+} else {
+    console.log("this is false");
+    
+}
